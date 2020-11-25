c5009e267cb7bbeb4ea13dcfc38414561e8134e5
Filip Gruczka <f.gruczka@actumlab.com>
2020-11-25 22:33:18 +0100

    Add base content to hobby component

--- src/app/hobby/hobby.component.ts
+++ src/app/hobby/hobby.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { ProfileService } from '../profile/profile.service';
+import { Location } from '@angular/common';
+import { Hobby } from '../profile/models/hobby.model';
 
 @Component({
   selector: 'app-hobby',
@@ -8,9 +13,30 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class HobbyComponent implements OnInit {
 
-  constructor() { }
+  hobby: Hobby;
+  loading = true;
+
+  constructor(
+    private route: ActivatedRoute,
+    private profileService: ProfileService,
+    private location: Location,
+    private changeDetectorRef: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
+    this.getHobby();
+  }
+
+  private getHobby(): void {
+    this.route.params.pipe(
+      switchMap(params => this.profileService.getHobby(params.hobby)),
+      tap(hobby => {
+        this.location.replaceState(hobby.name);
+        this.hobby = hobby;
+        this.loading = false;
+        this.changeDetectorRef.detectChanges();
+      }),
+    ).subscribe();
   }
 
 }
